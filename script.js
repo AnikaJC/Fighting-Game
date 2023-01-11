204508e3c650bd199577627de70d4b192fa2734c
Anika Jahan Choudhury <68474664+AnikaJC@users.noreply.github.com>
2023-01-11 14:18:39 +0000

    let's get started

---
+++ script.js
@@ -0,0 +1,100 @@
+
+let playButton = document.getElementById('play')
+let resultDiv = document.getElementById('result')
+let p1NameDiv = document.getElementById('p1Name')
+let p2NameDiv = document.getElementById('p2Name')
+let p1HealthDiv = document.getElementById('p1Health')
+let p2HealthDiv = document.getElementById('p2Health')
+
+
+const updateGame = (p1,p2,gameState) => {
+  
+}
+
+
+class Player {
+  constructor(name, health, attackDamage) {
+    this.name = name;
+    this.health = health;
+    this.attackDmg = attackDamage;
+  }
+  
+  strike (player, enemy, attackDmg) {
+    
+    
+
+  }
+ 
+  heal (player) {
+    
+    
+
+  }
+}
+
+
+class Game {
+  constructor() {
+    this.isOver = false;
+  }
+
+  
+  declareWinner(isOver,p1, p2) {
+    
+   
+
+  }
+
+  
+  reset(p1,p2) {
+   
+
+  }
+  
+  
+  play(p1, p2) {
+    
+    while (!this.isOver) {
+     
+    }
+    
+    
+  }
+
+}
+
+let player1 = new Player('Anika',100,10)
+console.log(player1.name)
+let player2 = new Player('Abir',100,10)
+let p1;
+let p2;
+
+
+let gameState;
+
+
+
+document.addEventListener('keydown', function(e) {
+ 
+
+});
+
+document.addEventListener('keydown', function(e) {
+  
+  
+});
+
+
+document.addEventListener('keydown', function(e) {
+  
+ 
+
+});
+
+document.addEventListener('keydown', function(e) {
+ 
+});
+
+
+
+
